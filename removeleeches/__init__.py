2527da15571cdad9ea4501eef5956a85e634826d
Xander Clinton <contact@xander479.co.uk>
2021-04-18 02:38:02 +0100

    Add confirmation dialog

--- removeleeches/__init__.py
+++ removeleeches/__init__.py
@@ -1,14 +1,19 @@
 from aqt import mw
 from aqt.qt import *
+from aqt.utils import showInfo
 
 
 def remove_leeches() -> None:
     ids = mw.col.findCards("tag:leech")
-    for id in ids:
-        card = mw.col.getCard(id)
-        card.note().delTag("leech")
-        card.note().flush()
-        card.flush()
+    if not ids:
+        showInfo("No leeches found.")
+    else:
+        for id in ids:
+            card = mw.col.getCard(id)
+            card.note().delTag("leech")
+            card.note().flush()
+            card.flush()
+            showInfo("Finished removing leeches.")
 
 
 action = QAction("Remove leeches", mw)
